1cb533342ce644d6459dc9f1798570f5382ace3d
Janet Vorobyeva <janet.vorobyeva@gmail.com>
2024-04-29 19:40:58 -0700

    Added UART support

--- src/main.c
+++ src/main.c
@@ -46,9 +46,9 @@
 
 void set_leds(bool led_state) {
 #ifdef AM_BSP_NUM_LEDS
-    uint32_t ui32GPIONumber;
+    //uint32_t ui32GPIONumber;
     for (uint32_t ux = 0; ux < AM_BSP_NUM_LEDS; ux++) {
-        ui32GPIONumber = am_bsp_psLEDs[ux].ui32GPIONumber;
+        //ui32GPIONumber = am_bsp_psLEDs[ux].ui32GPIONumber;
         (led_state) ? am_devices_led_on(am_bsp_psLEDs, ux) :
                       am_devices_led_off(am_bsp_psLEDs, ux);
     }
@@ -56,7 +56,121 @@ void set_leds(bool led_state) {
 }
 
 
+//*****************************************************************************
+//
+//      HAL ERROR HANDLING
+//
+//*****************************************************************************
 
+volatile uint32_t ui32LastError;
+void error_handler(uint32_t ui32ErrorStatus) {
+    ui32LastError = ui32ErrorStatus;
+    while (1);
+}
+#define CHECK_ERRORS(x) \
+    if ((x) != AM_HAL_STATUS_SUCCESS) {error_handler(x);}
+
+//*********************************************
+//
+//             UART configuration.
+//
+//*********************************************
+
+uint8_t g_pui8TxBuffer[256];
+uint8_t g_pui8RxBuffer[2];
+
+const am_hal_uart_config_t g_sUartConfig =
+{
+    // Standard UART settings: 115200-8-N-1
+    .ui32BaudRate = 115200,
+    .ui32DataBits    = AM_HAL_UART_DATA_BITS_8,
+    .ui32Parity      = AM_HAL_UART_PARITY_NONE,
+    .ui32StopBits    = AM_HAL_UART_ONE_STOP_BIT,
+    .ui32FlowControl = AM_HAL_UART_FLOW_CTRL_NONE,
+
+    // Set TX and RX FIFOs to interrupt at half-full.
+    .ui32FifoLevels = (AM_HAL_UART_TX_FIFO_1_2 |
+                       AM_HAL_UART_RX_FIFO_1_2),
+
+    // Buffers
+    .pui8TxBuffer = g_pui8TxBuffer,
+    .ui32TxBufferSize = sizeof(g_pui8TxBuffer),
+    .pui8RxBuffer = g_pui8RxBuffer,
+    .ui32RxBufferSize = sizeof(g_pui8RxBuffer),
+};
+
+// uart handle?
+void *phUART;
+
+
+//*********************************************
+//
+//             UART funcs
+//
+//*********************************************
+
+void uart_print(char *pcStr) {
+    uint32_t ui32StrLen = 0;
+    uint32_t ui32BytesWritten = 0;
+
+    // Measure the length of the string.
+    while (pcStr[ui32StrLen] != 0) { ui32StrLen++; }
+
+    // Print the string via the UART.
+    const am_hal_uart_transfer_t sUartWrite =
+    {
+        .ui32Direction = AM_HAL_UART_WRITE,
+        .pui8Data = (uint8_t *) pcStr,
+        .ui32NumBytes = ui32StrLen,
+        .ui32TimeoutMs = 0,
+        .pui32BytesTransferred = &ui32BytesWritten,
+    };
+
+    CHECK_ERRORS(am_hal_uart_transfer(phUART, &sUartWrite));
+
+    if (ui32BytesWritten != ui32StrLen) {
+        // Couldn't send the whole string!!
+        while(1); //ERROR
+    }
+}
+
+// Should override default am_uart_isr, defined in bsp I think?
+void am_uart_isr(void) {
+    //
+    // Service the FIFOs as necessary, and clear the interrupts.
+    //
+    uint32_t ui32Status, ui32Idle;
+    am_hal_uart_interrupt_status_get(phUART, &ui32Status, true);
+    am_hal_uart_interrupt_clear(phUART, ui32Status);
+    am_hal_uart_interrupt_service(phUART, ui32Status, &ui32Idle);
+}
+
+
+void uart_init() {
+
+    // initialize the uart
+    CHECK_ERRORS(am_hal_uart_initialize(0, &phUART));
+    CHECK_ERRORS(am_hal_uart_power_control(phUART, AM_HAL_SYSCTRL_WAKE, false));
+    CHECK_ERRORS(am_hal_uart_configure(phUART, &g_sUartConfig));
+
+    // Set the uart pins to their configs (specified in sfe/.../am_bsp_pins.h)
+    am_hal_gpio_pinconfig(AM_BSP_GPIO_COM_UART_TX, g_AM_BSP_GPIO_COM_UART_TX);
+    am_hal_gpio_pinconfig(AM_BSP_GPIO_COM_UART_RX, g_AM_BSP_GPIO_COM_UART_RX);
+
+    // Enable interrupts? (AM_BSP_UART_PRINT_INST is 0 I think?)
+    NVIC_EnableIRQ((IRQn_Type)(UART0_IRQn + AM_BSP_UART_PRINT_INST));
+    am_hal_interrupt_master_enable();
+
+    // Set the main print interface to use the UART print function we defined.
+    am_util_stdio_printf_init(uart_print);
+}
+
+
+//*********************************************
+//
+//                    main
+//
+//*********************************************
 
 int main(void)
 {
@@ -84,12 +198,22 @@ int main(void)
 
 	//// After init is done, enable interrupts
 	//am_hal_interrupt_master_enable();
+    
+
+    // === Setup UART, send hello
+    uart_init();
+
+    am_util_stdio_terminal_clear();
+    am_util_stdio_printf("Hello World! (Over UART!)\n\n");
+    am_hal_uart_tx_flush(phUART);
+
 
     init_morse_table();
 
     bool led_state = false;
     set_leds(led_state);
 
+    // === Setup Morse
     char *morse_str = " Test 1 2 3. the war of 1812. jabberwocky."
                       " we hold these truths to be self evident. "
                       " 4 score and 7 years ago."
@@ -103,19 +227,25 @@ int main(void)
     state.p_str = morse_str;
 
     while (1) {
-      am_hal_gpio_state_write(JV_CS_PIN, AM_HAL_GPIO_OUTPUT_TOGGLE);
+        am_hal_gpio_state_write(JV_CS_PIN, AM_HAL_GPIO_OUTPUT_TOGGLE);
 
-      struct morse_output result = morseAdvance(&state);
+        int last_offset = state.char_offset;
+        struct morse_output result = morseAdvance(&state);
+        if((int)state.char_offset != last_offset) {
+            am_util_stdio_printf("%c", state.p_str[last_offset]);
+        }
 
-  
-      if(result.valid) {
-        set_leds(result.out_high);
-        am_util_delay_ms(result.duration_ms);
 
-      } else { //end of string, reset
-        state.char_offset = 0;
-        am_util_delay_ms(BLINK_PERIOD);
-      }
+
+    
+        if(result.valid) {
+            set_leds(result.out_high);
+            am_util_delay_ms(result.duration_ms);
+
+        } else { //end of string, reset
+            state.char_offset = 0;
+            am_util_delay_ms(BLINK_PERIOD);
+        }
     }
 
     // Blink forever

21287bb38367c23e7adbeaf2a029f2cfdc4e3b21
Janet Vorobyeva <janet.vorobyeva@gmail.com>
2024-06-09 14:25:56 -0700

    Got radio spi comms tested on CP/S Rev A board

--- src/main.c
+++ src/main.c
@@ -26,9 +26,26 @@
 
 #define BLINK_PERIOD 2000
 
-//#define JV_CS_PIN 28
-#define JV_RHEO_CS_CHAN 0 //pin 11 on IOM0
-#define JV_LORA_CS_CHAN 1 //pin 17 I believe on IOM0
+
+//===== PIN ASSIGNMENTS For: CPS board mk1 revA
+#define JV_LORA_CS_CHAN 0 //GPIO 11, on IOM0
+//#define JV_LORA_CS_CHAN 1 //TEMP DEBUG: GPIO 17, on IOM0
+
+#define JV_PIN_LED 0 //GPIO 0
+
+#define JV_PIN_LORA_EN 36
+#define JV_PIN_LORA_RST 3
+#define JV_PIN_LORA_DI0 38
+
+#define JV_PIN_ADP_PGOOD 39
+#define JV_PIN_ADP_DIS_SW 49
+
+#define JV_PIN_ADC_VIN  16 // ADC0/TRIG0/CMPIN0
+#define JV_PIN_ADC_VBAT 31 // ADC3
+#define JV_PIN_ADC_DP0N 12 
+#define JV_PIN_ADC_DP0P 13 
+#define JV_PIN_ADC_DP1N 15  //yes 15&14 are swapped
+#define JV_PIN_ADC_DP1P 14 
 
 // __attribute__((constructor))
 // static void redboard_init(void)
@@ -46,15 +63,20 @@
 // }
 
 
+//void set_leds(bool led_state) {
+//#ifdef AM_BSP_NUM_LEDS
+//    //uint32_t ui32GPIONumber;
+//    for (uint32_t ux = 0; ux < AM_BSP_NUM_LEDS; ux++) {
+//        //ui32GPIONumber = am_bsp_psLEDs[ux].ui32GPIONumber;
+//        (led_state) ? am_devices_led_on(am_bsp_psLEDs, ux) :
+//                      am_devices_led_off(am_bsp_psLEDs, ux);
+//    }
+//#endif // AM_BSP_NUM_LEDS
+//}
+
 void set_leds(bool led_state) {
-#ifdef AM_BSP_NUM_LEDS
-    //uint32_t ui32GPIONumber;
-    for (uint32_t ux = 0; ux < AM_BSP_NUM_LEDS; ux++) {
-        //ui32GPIONumber = am_bsp_psLEDs[ux].ui32GPIONumber;
-        (led_state) ? am_devices_led_on(am_bsp_psLEDs, ux) :
-                      am_devices_led_off(am_bsp_psLEDs, ux);
-    }
-#endif // AM_BSP_NUM_LEDS
+    (led_state) ? am_hal_gpio_state_write(JV_PIN_LED, AM_HAL_GPIO_OUTPUT_SET) :
+                  am_hal_gpio_state_write(JV_PIN_LED, AM_HAL_GPIO_OUTPUT_CLEAR);
 }
 
 
@@ -78,7 +100,8 @@ void error_handler(uint32_t ui32ErrorStatus) {
 //
 //*********************************************
 #define IOMN (0)
-#define SPI_MODE (AM_HAL_IOM_SPI_MODE_3)
+//#define SPI_MODE (AM_HAL_IOM_SPI_MODE_3) 
+#define SPI_MODE (AM_HAL_IOM_SPI_MODE_0)
 //#define SPI_FREQ (AM_HAL_IOM_4MHZ)
 #define SPI_FREQ (AM_HAL_IOM_10KHZ)
 void* iom_handle = NULL;
@@ -133,21 +156,91 @@ void init_iom( void ){
 }
 
 
-// for MCP4162
-// register in 0-15, gives back 10-bit result
-int  spi_read_reg(int reg) {
-    reg = reg & 0xF;
+// // for MCP4162
+// // register in 0-15, gives back 10-bit result
+// int  spi_read_reg(int reg) {
+//     reg = reg & 0xF;
+// 
+//     uint8_t cmd[4];
+//     uint8_t rx_buf[4];
+//     cmd[0] = (reg << 4) | 0xC; // rrrr 11 .., 11 is read
+//     cmd[1] = 0;
+// 
+//     //NOTE: i think IOM0 CS0 might be pin 11? see bsps
+//     xfer.uPeerInfo.ui32SpiChipSelect = JV_RHEO_CS_CHAN;
+//     xfer.ui32InstrLen = 0;
+//     xfer.ui32Instr = 0;
+//     xfer.ui32NumBytes = 2;
+//     xfer.eDirection = AM_HAL_IOM_FULLDUPLEX;
+//     xfer.pui32TxBuffer = (uint32_t*)cmd;
+//     xfer.pui32RxBuffer = (uint32_t*)rx_buf;
+//     xfer.bContinue = false;
+//     xfer.ui8RepeatCount = 0;
+//     xfer.ui8Priority = 1;
+//     xfer.ui32PauseCondition = 0;
+//     xfer.ui32StatusSetClr = 0;
+// 
+//     //am_util_stdio_printf("reading reg %d:\n", reg);
+//     uint32_t status = AM_HAL_STATUS_SUCCESS;
+//     status = am_hal_iom_spi_blocking_fullduplex(iom_handle, &xfer);
+//     if(status != AM_HAL_STATUS_SUCCESS) { report(status);}
+// 
+//     int result = ((uint32_t)(rx_buf[0] & 0x1) << 8) | rx_buf[1];
+//     return result;
+// }
+// 
+// // for MCP4162
+// // register in 0-15, 
+// // value up to 9 bits
+// int  spi_write_reg(int reg, int value) {
+//     reg = reg & 0xF;
+// 
+//     uint8_t cmd[4];
+//     uint8_t rx_buf[4];
+//     cmd[0] = (reg << 4) | ((value >> 8) &0x1); 
+//     // rrrr 00 .d, 00 is write, d is bit 8
+//     cmd[1] = (value & 0xff);
+// 
+//     //NOTE: i think IOM0 CS0 might be pin 11? see bsps
+//     xfer.uPeerInfo.ui32SpiChipSelect = JV_RHEO_CS_CHAN;
+//     xfer.ui32InstrLen = 0;
+//     xfer.ui32Instr = 0;
+//     xfer.ui32NumBytes = 2;
+//     xfer.eDirection = AM_HAL_IOM_FULLDUPLEX;
+//     xfer.pui32TxBuffer = (uint32_t*)cmd;
+//     xfer.pui32RxBuffer = (uint32_t*)rx_buf;
+//     xfer.bContinue = false;
+//     xfer.ui8RepeatCount = 0;
+//     xfer.ui8Priority = 1;
+//     xfer.ui32PauseCondition = 0;
+//     xfer.ui32StatusSetClr = 0;
+// 
+//     //am_util_stdio_printf("reading reg %d:\n", reg);
+//     uint32_t status = AM_HAL_STATUS_SUCCESS;
+//     status = am_hal_iom_spi_blocking_fullduplex(iom_handle, &xfer);
+//     if(status != AM_HAL_STATUS_SUCCESS) { report(status);}
+// 
+//     int result = ((uint32_t)(rx_buf[0] & 0x1) << 8) | rx_buf[1];
+//     return result;
+// }
 
-    uint8_t cmd[4];
-    uint8_t rx_buf[4];
-    cmd[0] = (reg << 4) | 0xC; // rrrr 11 .., 11 is read
+
+// for RFM95W
+// 7bit reg addr, high bit is W/NR
+// nbytes must be <255
+// DEBUG: RETURNS NOTHING
+void  spi_read_lora_nregs(int reg, int nbytes) {
+
+    uint8_t cmd[256] = {};
+    uint8_t rx_buf[256] = {};
+    cmd[0] = reg & 0x7F; //high bit 0 for read
     cmd[1] = 0;
 
     //NOTE: i think IOM0 CS0 might be pin 11? see bsps
-    xfer.uPeerInfo.ui32SpiChipSelect = JV_RHEO_CS_CHAN;
+    xfer.uPeerInfo.ui32SpiChipSelect = JV_LORA_CS_CHAN;
     xfer.ui32InstrLen = 0;
     xfer.ui32Instr = 0;
-    xfer.ui32NumBytes = 2;
+    xfer.ui32NumBytes = 1+nbytes;
     xfer.eDirection = AM_HAL_IOM_FULLDUPLEX;
     xfer.pui32TxBuffer = (uint32_t*)cmd;
     xfer.pui32RxBuffer = (uint32_t*)rx_buf;
@@ -162,43 +255,7 @@ int  spi_read_reg(int reg) {
     status = am_hal_iom_spi_blocking_fullduplex(iom_handle, &xfer);
     if(status != AM_HAL_STATUS_SUCCESS) { report(status);}
 
-    int result = ((uint32_t)(rx_buf[0] & 0x1) << 8) | rx_buf[1];
-    return result;
-}
-
-// for MCP4162
-// register in 0-15, 
-// value up to 9 bits
-int  spi_write_reg(int reg, int value) {
-    reg = reg & 0xF;
-
-    uint8_t cmd[4];
-    uint8_t rx_buf[4];
-    cmd[0] = (reg << 4) | ((value >> 8) &0x1); 
-    // rrrr 00 .d, 00 is write, d is bit 8
-    cmd[1] = (value & 0xff);
-
-    //NOTE: i think IOM0 CS0 might be pin 11? see bsps
-    xfer.uPeerInfo.ui32SpiChipSelect = JV_RHEO_CS_CHAN;
-    xfer.ui32InstrLen = 0;
-    xfer.ui32Instr = 0;
-    xfer.ui32NumBytes = 2;
-    xfer.eDirection = AM_HAL_IOM_FULLDUPLEX;
-    xfer.pui32TxBuffer = (uint32_t*)cmd;
-    xfer.pui32RxBuffer = (uint32_t*)rx_buf;
-    xfer.bContinue = false;
-    xfer.ui8RepeatCount = 0;
-    xfer.ui8Priority = 1;
-    xfer.ui32PauseCondition = 0;
-    xfer.ui32StatusSetClr = 0;
-
-    //am_util_stdio_printf("reading reg %d:\n", reg);
-    uint32_t status = AM_HAL_STATUS_SUCCESS;
-    status = am_hal_iom_spi_blocking_fullduplex(iom_handle, &xfer);
-    if(status != AM_HAL_STATUS_SUCCESS) { report(status);}
-
-    int result = ((uint32_t)(rx_buf[0] & 0x1) << 8) | rx_buf[1];
-    return result;
+    //return rx_buf[1]; //the 0th byte was the addr, the 1st byte is resp
 }
 
 
@@ -288,6 +345,33 @@ int main(void)
 	//// After init is done, enable interrupts
 	//am_hal_interrupt_master_enable();
     
+    // == Enable LED output, 2mA drive
+    am_hal_gpio_pinconfig(JV_PIN_LED,  g_AM_HAL_GPIO_OUTPUT);
+
+    //Enable Lora (LORA_EN high, RESET low)
+    am_hal_gpio_pinconfig(JV_PIN_LORA_EN,     g_AM_HAL_GPIO_OUTPUT);
+    am_hal_gpio_state_write(JV_PIN_LORA_EN, AM_HAL_GPIO_OUTPUT_SET);
+
+    am_hal_gpio_pinconfig(JV_PIN_LORA_RST,    g_AM_HAL_GPIO_OUTPUT);
+    //am_hal_gpio_state_write(JV_PIN_LORA_RST, AM_HAL_GPIO_OUTPUT_CLEAR);
+    am_hal_gpio_state_write(JV_PIN_LORA_RST, AM_HAL_GPIO_OUTPUT_SET);
+
+    am_hal_gpio_pinconfig(JV_PIN_LORA_DI0,    g_AM_HAL_GPIO_INPUT);
+
+    //Setup ADP Pins to do nothing (DIS_SW low)
+    am_hal_gpio_pinconfig(JV_PIN_ADP_PGOOD,   g_AM_HAL_GPIO_INPUT);
+    am_hal_gpio_pinconfig(JV_PIN_ADP_DIS_SW,  g_AM_HAL_GPIO_OUTPUT);
+    am_hal_gpio_state_write(JV_PIN_ADP_DIS_SW, AM_HAL_GPIO_OUTPUT_CLEAR);
+
+    // Setup ADC pins as inputs
+    am_hal_gpio_pinconfig(JV_PIN_ADC_VIN,     g_AM_HAL_GPIO_INPUT);
+    am_hal_gpio_pinconfig(JV_PIN_ADC_VBAT,    g_AM_HAL_GPIO_INPUT);
+    am_hal_gpio_pinconfig(JV_PIN_ADC_VBAT,    g_AM_HAL_GPIO_INPUT);
+    am_hal_gpio_pinconfig(JV_PIN_ADC_DP0N,    g_AM_HAL_GPIO_INPUT);
+    am_hal_gpio_pinconfig(JV_PIN_ADC_DP0P,    g_AM_HAL_GPIO_INPUT);
+    am_hal_gpio_pinconfig(JV_PIN_ADC_DP1N,    g_AM_HAL_GPIO_INPUT);
+    am_hal_gpio_pinconfig(JV_PIN_ADC_DP1P,    g_AM_HAL_GPIO_INPUT);
+
 
     
 
@@ -308,7 +392,6 @@ int main(void)
     am_hal_gpio_pinconfig(AM_BSP_GPIO_ITM_SWO, g_AM_BSP_GPIO_ITM_SWO);
     // Attach the ITM to the STDIO driver.
     am_util_stdio_printf_init(am_hal_itm_print);
-
     // ======== END MANUAL ITM
 
     //am_util_stdio_terminal_clear();
@@ -316,32 +399,44 @@ int main(void)
     //am_hal_uart_tx_flush(phUART);
 
 
+    bool led_state = false;
     
-    while(1){
-        am_util_stdio_printf("Hello!\n");
-        am_util_delay_ms(2000);
+    // while(1){
+    //     am_util_stdio_printf("Hello!\n");
 
-    } // TODO TEMP: CHECK PRINTF
+    //     led_state = !led_state;
+    //     set_leds(led_state);
+
+    //     am_util_delay_ms(2000);
+
+    // } // TODO TEMP: CHECK PRINTF
     
 
     //===== setup iom, read some regs
     init_iom();
-    am_util_stdio_printf("READING RHEO REGS\n");
-    for (int reg = 0; reg < 8; reg++) {
-        int res = spi_read_reg(reg);
-        am_util_stdio_printf("reg %d: 0x%x\n", reg, res);
-    }
+    am_util_stdio_printf("READING INITIAL REGOPMODE\n");
+    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", 1, spi_read_lora_reg(1));
+    am_util_stdio_printf("lora version (@0x%x): 0x%x\n", 0x42, spi_read_lora_reg(0x42));
 
-    
-    int val = 10;
-    am_util_stdio_printf("writing reg0 := %d\n",val);
-    spi_write_reg(0, val);
+    //===== try resetting the Lora module
+    am_util_stdio_printf("RESETTING LORA MODULE:\n");
+    am_hal_gpio_state_write(JV_PIN_LORA_RST, AM_HAL_GPIO_OUTPUT_CLEAR);
+    am_util_delay_ms(50);
+    am_hal_gpio_state_write(JV_PIN_LORA_RST, AM_HAL_GPIO_OUTPUT_SET);
+    am_util_delay_ms(50);
 
-    for (int reg = 0; reg < 8; reg++) {
-        int res = spi_read_reg(reg);
-        am_util_stdio_printf("reg %d: 0x%x\n", reg, res);
-    }
+    //am_util_stdio_printf("READING LORA REGS:\n");
+    //for (int reg = 0; reg < 8; reg++) {
+    //    int res = spi_read_lora_reg(reg);
+    //    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", reg, res);
+    //}
 
+    am_util_stdio_printf("Trying burst read LORA REGS:\n");
+    spi_read_lora_nregs(0x41, 0x42); //read first 15 bytes? skipping addr 0
+
+    am_util_stdio_printf("Trying Wakeup\n");
+    spi_write_lora_reg(0x1, 0x89); //RegOpMode, set to sleep
+    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", 1, spi_read_lora_reg(1));
 
     am_util_stdio_printf("Enabling LORA MODE\n");
     spi_write_lora_reg(0x1, 0x08); //RegOpMode, set to sleep
@@ -357,21 +452,8 @@ int main(void)
     }
 
 
-
-    am_util_stdio_printf("STARTING RHEO LOOP\n");
-    // Let's change the resistance gradually, once per second
-    val = 0;
-    while(1) {
-        val = (val+16) &0xff;
-        am_util_stdio_printf("writing reg0 := %d\n",val);
-        spi_write_reg(0, val);
-        am_util_delay_ms(5000);
-    }
-
-
     init_morse_table();
 
-    bool led_state = false;
     set_leds(led_state);
 
     // === Setup Morse
@@ -395,7 +477,6 @@ int main(void)
         if((int)state.char_offset != last_offset) {
             am_util_stdio_printf("%c", state.p_str[last_offset]);
         }
-
 
 
     

c997269cd0965d71dcd47b1c2f9b3afd7eae8180
Janet Vorobyeva <janet.vorobyeva@gmail.com>
2024-06-10 21:20:43 -0700

    Got LORA working, sends packet, code cleaning
    Transmits a short packet every 2 seconds
    Cleaned up code a bit

--- src/main.c
+++ src/main.c
@@ -93,156 +93,38 @@ void error_handler(uint32_t ui32ErrorStatus) {
 //                 SPI STUFF
 //
 //*********************************************
-#define IOMN (0)
-//#define SPI_MODE (AM_HAL_IOM_SPI_MODE_3) 
-#define SPI_MODE (AM_HAL_IOM_SPI_MODE_0)
-//#define SPI_FREQ (AM_HAL_IOM_4MHZ)
-#define SPI_FREQ (AM_HAL_IOM_10KHZ)
-void* iom_handle = NULL;
-am_hal_iom_config_t iom_cfg = {0};
-am_hal_iom_transfer_t xfer = {0};
-
 #define report(s) am_util_stdio_printf("status: 0x%08X (function: %s, file: %s, line: %d)\n", s, __func__, __FILE__, __LINE__)
 
-void init_iom( void ){
-    uint32_t status = AM_HAL_STATUS_SUCCESS;
 
-    iom_cfg.eInterfaceMode = AM_HAL_IOM_SPI_MODE;
-    iom_cfg.ui32ClockFreq = SPI_FREQ;
-    iom_cfg.eSpiMode = SPI_MODE;
-    iom_cfg.pNBTxnBuf = NULL;
-    iom_cfg.ui32NBTxnBufLength = 0;
-
-    status = am_hal_iom_initialize(IOMN, &iom_handle);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }
-
-    status = am_hal_iom_power_ctrl(iom_handle, AM_HAL_SYSCTRL_WAKE, false);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }
-
-    status = am_hal_iom_configure(iom_handle, &iom_cfg);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }
-
-    status = am_hal_iom_enable(iom_handle);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }
-
-    // config pins
-    status = am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM0_MISO, g_AM_BSP_GPIO_IOM0_MISO);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }
-
-    status = am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM0_MOSI, g_AM_BSP_GPIO_IOM0_MOSI);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }
-
-    status = am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM0_SCK, g_AM_BSP_GPIO_IOM0_SCK);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }\
-
-    //JV: added this: initialize chip select pins
-    status = am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM0_CS, g_AM_BSP_GPIO_IOM0_CS);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }\
-
-    status = am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM0_CS1, g_AM_BSP_GPIO_IOM0_CS1);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }\
-
-    status = am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM0_CS2, g_AM_BSP_GPIO_IOM0_CS2);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }\
-
-    status = am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM0_CS3, g_AM_BSP_GPIO_IOM0_CS3);
-    if(status != AM_HAL_STATUS_SUCCESS){ report(status); }\
-}
-
-
-// for RFM95W
-// 7bit reg addr, high bit is W/NR
-// nbytes must be <255
-// DEBUG: RETURNS NOTHING
-void  spi_read_lora_nregs(int reg, int nbytes) {
-
-    uint8_t cmd[256] = {};
-    uint8_t rx_buf[256] = {};
-    cmd[0] = reg & 0x7F; //high bit 0 for read
-    cmd[1] = 0;
-
-    //NOTE: i think IOM0 CS0 might be pin 11? see bsps
-    xfer.uPeerInfo.ui32SpiChipSelect = JV_LORA_CS_CHAN;
-    xfer.ui32InstrLen = 0;
-    xfer.ui32Instr = 0;
-    xfer.ui32NumBytes = 1+nbytes;
-    xfer.eDirection = AM_HAL_IOM_FULLDUPLEX;
-    xfer.pui32TxBuffer = (uint32_t*)cmd;
-    xfer.pui32RxBuffer = (uint32_t*)rx_buf;
-    xfer.bContinue = false;
-    xfer.ui8RepeatCount = 0;
-    xfer.ui8Priority = 1;
-    xfer.ui32PauseCondition = 0;
-    xfer.ui32StatusSetClr = 0;
-
-    //am_util_stdio_printf("reading reg %d:\n", reg);
-    uint32_t status = AM_HAL_STATUS_SUCCESS;
-    status = am_hal_iom_spi_blocking_fullduplex(iom_handle, &xfer);
-    if(status != AM_HAL_STATUS_SUCCESS) { report(status);}
-
-    //return rx_buf[1]; //the 0th byte was the addr, the 1st byte is resp
-}
-
-
-// for RFM95W
-// 7bit reg addr, high bit is W/NR
-int  spi_read_lora_reg(int reg) {
-    uint8_t cmd[4];
-    uint8_t rx_buf[4];
-    cmd[0] = reg & 0x7F; //high bit 0 for read
-    cmd[1] = 0;
-
-    //NOTE: i think IOM0 CS0 might be pin 11? see bsps
-    xfer.uPeerInfo.ui32SpiChipSelect = JV_LORA_CS_CHAN;
-    xfer.ui32InstrLen = 0;
-    xfer.ui32Instr = 0;
-    xfer.ui32NumBytes = 2;
-    xfer.eDirection = AM_HAL_IOM_FULLDUPLEX;
-    xfer.pui32TxBuffer = (uint32_t*)cmd;
-    xfer.pui32RxBuffer = (uint32_t*)rx_buf;
-    xfer.bContinue = false;
-    xfer.ui8RepeatCount = 0;
-    xfer.ui8Priority = 1;
-    xfer.ui32PauseCondition = 0;
-    xfer.ui32StatusSetClr = 0;
-
-    //am_util_stdio_printf("reading reg %d:\n", reg);
-    uint32_t status = AM_HAL_STATUS_SUCCESS;
-    status = am_hal_iom_spi_blocking_fullduplex(iom_handle, &xfer);
-    if(status != AM_HAL_STATUS_SUCCESS) { report(status);}
-
-    return rx_buf[1]; //the 0th byte was the addr, the 1st byte is resp
-}
-
-// for RFM95W
-// 7bit reg addr, high bit is W/NR
-void spi_write_lora_reg(int reg, int val) {
-    uint8_t cmd[4];
-    uint8_t rx_buf[4];
-    cmd[0] = (reg & 0x7F) | 0x80; //high bit 1 for write
-    cmd[1] = val & 0xFF;
-
-    //NOTE: i think IOM0 CS0 might be pin 11? see bsps
-    xfer.uPeerInfo.ui32SpiChipSelect = JV_LORA_CS_CHAN;
-    xfer.ui32InstrLen = 0;
-    xfer.ui32Instr = 0;
-    xfer.ui32NumBytes = 2;
-    xfer.eDirection = AM_HAL_IOM_FULLDUPLEX;
-    xfer.pui32TxBuffer = (uint32_t*)cmd;
-    xfer.pui32RxBuffer = (uint32_t*)rx_buf;
-    xfer.bContinue = false;
-    xfer.ui8RepeatCount = 0;
-    xfer.ui8Priority = 1;
-    xfer.ui32PauseCondition = 0;
-    xfer.ui32StatusSetClr = 0;
-
-    //am_util_stdio_printf("reading reg %d:\n", reg);
-    uint32_t status = AM_HAL_STATUS_SUCCESS;
-    status = am_hal_iom_spi_blocking_fullduplex(iom_handle, &xfer);
-    if(status != AM_HAL_STATUS_SUCCESS) { report(status);}
-
-    //return rx_buf[1]; //the 0th byte was the addr, the 1st byte is resp
-}
+// // for RFM95W
+// // 7bit reg addr, high bit is W/NR
+// void spi_write_lora_reg(int reg, int val) {
+//     uint8_t cmd[4];
+//     uint8_t rx_buf[4];
+//     cmd[0] = (reg & 0x7F) | 0x80; //high bit 1 for write
+//     cmd[1] = val & 0xFF;
+// 
+//     //NOTE: i think IOM0 CS0 might be pin 11? see bsps
+//     xfer.uPeerInfo.ui32SpiChipSelect = JV_LORA_CS_CHAN;
+//     xfer.ui32InstrLen = 0;
+//     xfer.ui32Instr = 0;
+//     xfer.ui32NumBytes = 2;
+//     xfer.eDirection = AM_HAL_IOM_FULLDUPLEX;
+//     xfer.pui32TxBuffer = (uint32_t*)cmd;
+//     xfer.pui32RxBuffer = (uint32_t*)rx_buf;
+//     xfer.bContinue = false;
+//     xfer.ui8RepeatCount = 0;
+//     xfer.ui8Priority = 1;
+//     xfer.ui32PauseCondition = 0;
+//     xfer.ui32StatusSetClr = 0;
+// 
+//     //am_util_stdio_printf("reading reg %d:\n", reg);
+//     uint32_t status = AM_HAL_STATUS_SUCCESS;
+//     status = am_hal_iom_spi_blocking_fullduplex(iom_handle, &xfer);
+//     if(status != AM_HAL_STATUS_SUCCESS) { report(status);}
+// 
+//     //return rx_buf[1]; //the 0th byte was the addr, the 1st byte is resp
+// }
 
 uint8_t tmp_read_reg(struct spi_device *device, uint8_t addr){
     uint8_t buff_val;
@@ -253,6 +135,206 @@ uint8_t tmp_read_reg(struct spi_device *device, uint8_t addr){
 void tmp_write_reg(struct spi_device *device, uint8_t addr, uint8_t val){
     uint8_t buff_val = val;
     spi_device_cmd_write(device, addr | 0x80, &buff_val, 1);
+}
+
+//*********************************************
+//
+//          Test functions (inf loops)
+//
+//*********************************************
+
+// Prints hello world and blinks
+void testprog_helloblinky() {
+    am_util_stdio_printf("Starting Blinky\n");
+
+    bool led_state = 0;
+    while(1){
+        am_util_stdio_printf("Hello!\n");
+
+        led_state = !led_state;
+        set_leds(led_state);
+
+        am_util_delay_ms(2000);
+
+    }
+}
+
+// Initializes LORA module and sends a packet every few seconds
+void testprog_lora() {
+    am_util_stdio_printf("Starting LORA test program:\n");
+
+    //*********************************************
+    //           INITIALIZE Lora module
+    //*********************************************
+    
+    // Asimple spi init
+    struct spi_bus* spi_bus_0;
+    struct spi_device* lora_spi; // handle to the spi device for the lora module
+    struct lora lora_storage = {};
+    struct lora* lora_obj = &lora_storage; // handle for the lora module, wraps the spi device
+                                           
+    //===== try resetting the Lora module (active low reset)
+    //const int CONF_SPI_FREQ = 10000000; //10MHz
+    const int CONF_SPI_FREQ = 10000; //10KHz
+    const uint32_t CONF_LORA_FREQ = 902300000u;
+    //LORA_FREQ = 915000000; //915MHZ? TODO: pick a channel?
+                           //902.3MHZ? chan 0
+                           
+    const uint8_t CONF_LORA_SF = 7; // min 6, fast, noisy, max 12, slow, robust
+
+    // 0b0111: 125KHz bandwidth, should be for channels 0-63 in US915? 
+    const uint8_t CONF_LORA_BW = 7;
+
+    //0b100: 4/8 coding (2x overhead?)
+    const uint8_t CONF_LORA_CODERATE = 4;
+
+    am_util_stdio_printf("RESETTING LORA MODULE:\n");
+    am_hal_gpio_state_write(JV_PIN_LORA_RST, AM_HAL_GPIO_OUTPUT_CLEAR);
+    am_util_delay_ms(20);
+    am_hal_gpio_state_write(JV_PIN_LORA_RST, AM_HAL_GPIO_OUTPUT_SET);
+    am_util_delay_ms(10);
+
+
+    // Init LORA as spi device, 10MHz
+    spi_bus_0 = spi_bus_get_instance(SPI_BUS_0);
+    lora_spi = spi_device_get_instance(spi_bus_0, SPI_CS_0, CONF_SPI_FREQ);
+
+    if(!spi_bus_enable(spi_bus_0)) { report(-1); }
+
+    tmp_read_reg(lora_spi, 0x01);
+
+
+    am_util_stdio_printf("Initializing asimple LORA\n");
+    if(!lora_init(lora_obj, lora_spi, CONF_LORA_FREQ, JV_PIN_LORA_DI0)) { report(-1); }
+    
+    if(!lora_set_spreading_factor(lora_obj, CONF_LORA_SF)) { report(-1); } 
+    lora_set_bandwidth(lora_obj, CONF_LORA_BW); 
+    lora_set_coding_rate(lora_obj, CONF_LORA_CODERATE); 
+
+    //*********************************************
+    //           Initialization complete
+    //*********************************************
+
+    am_util_stdio_printf("READING INITIAL REGOPMODE\n");
+    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", 1, tmp_read_reg(lora_spi, 0x01));
+    am_util_stdio_printf("lora version (@0x%x): 0x%x\n", 0x42, tmp_read_reg(lora_spi, 0x42));
+
+    //am_util_stdio_printf("READING LORA REGS:\n");
+    //for (int reg = 0; reg < 8; reg++) {
+    //    int res = spi_read_lora_reg(reg);
+    //    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", reg, res);
+    //}
+
+    //am_util_stdio_printf("Trying burst read LORA REGS:\n");
+    //spi_read_lora_nregs(0x41, 0x42); //read first 15 bytes? skipping addr 0
+
+    //am_util_stdio_printf("Trying Wakeup\n");
+    //tmp_write_reg(lora_spi, 0x1, 0x89); //RegOpMode, set to sleep
+    //am_util_stdio_printf("lora reg 0x%x: 0x%x\n", 1, tmp_read_reg(lora_spi, 1));
+
+    //am_util_stdio_printf("Enabling LORA MODE\n");
+    //tmp_write_reg(lora_spi, 0x1, 0x08); //RegOpMode, set to sleep
+    //am_util_stdio_printf("lora reg 0x%x: 0x%x\n", 1, tmp_read_reg(lora_spi, 1));
+    //tmp_write_reg(lora_spi, 0x1, 0x88); //RegOpMode, enable lora
+    //tmp_write_reg(lora_spi, 0x1, 0x89); //RegOpMode, wake to stdby mode
+    //set to lora mode (bit 7)
+    //set to Low freq mode (bit3 hi)
+    //set to stdby mode (low bits 001)
+    //
+    am_util_stdio_printf("READING LORA REGS:\n");
+    for (int reg = 0; reg < 8; reg++) {
+        //int res = spi_read_lora_reg(reg);
+        int res = tmp_read_reg(lora_spi, reg);
+        am_util_stdio_printf("lora reg 0x%x: 0x%x\n", reg, res);
+    }
+
+    // ==== Trying to send a packet
+    
+    
+    int num_packets = 0;
+    int len = 0;
+    int tx_bytes = 0;
+    //am_util_stdio_printf("LORA tx_addr = 0x%x\n", lora_obj->tx_addr);
+
+    while(1) {
+
+        // Blink on
+        set_leds(true);
+        am_util_stdio_printf("Sending packet %d...\n", num_packets);
+
+        // Fmt Packet
+        unsigned char tx_buff[1024] = {};
+        len = am_util_stdio_snprintf((char *)tx_buff, sizeof(tx_buff)-1, 
+                "TX Test, packet #%d\n", num_packets);
+
+        // Can send at most 0x80 bytes (possibly 255 if modify asimple?)
+        if(len >= 0x80) { 
+            am_util_stdio_printf("(Truncated packet from %d bytes to 127)\n", len); 
+            len = 0x7F; 
+        }
+
+        // Transmit
+        tx_bytes = (int)lora_send_packet(lora_obj, tx_buff, len);
+        if (tx_bytes != len) {
+            am_util_stdio_printf("Error: Only sent %d bytes, expected %d!\n", 
+                    tx_bytes, len);
+        } else {
+            am_util_stdio_printf("Sent %d bytes!\n", tx_bytes);
+        }
+
+        num_packets++;
+
+        set_leds(false);
+
+        am_util_delay_ms(2000);
+    }
+}
+
+// Blinks the LED transmitting a long morse code string
+void testprog_morse() {
+    am_util_stdio_printf("Starting Morse code program:\n");
+
+    bool led_state = false;
+    
+    char *morse_str = " Test 1 2 3. the war of 1812. jabberwocky."
+                      " we hold these truths to be self evident. "
+                      " 4 score and 7 years ago."
+    " to be or not to be, that is the question. whether tis nobler in the mind"
+    " to suffer the slings and arrows of outrageous fortune, or to take arms"
+    " against a sea of troubles and by opposing end them\0"
+    ;
+
+
+    init_morse_table();
+
+    set_leds(led_state);
+
+    // === Setup Morse
+
+    // Initialize morse_state to be at first phase of first char of str
+    struct morse_state state = {0};
+    state.p_str = morse_str;
+
+    while (1) {
+        //am_hal_gpio_state_write(JV_CS_PIN, AM_HAL_GPIO_OUTPUT_TOGGLE);
+
+        int last_offset = state.char_offset;
+        struct morse_output result = morseAdvance(&state);
+        if((int)state.char_offset != last_offset) {
+            am_util_stdio_printf("%c", state.p_str[last_offset]);
+        }
+
+
+    
+        if(result.valid) {
+            set_leds(result.out_high);
+            am_util_delay_ms(result.duration_ms);
+
+        } else { //end of string, reset
+            state.char_offset = 0;
+            am_util_delay_ms(BLINK_PERIOD);
+        }
+    }
 }
 
 //*********************************************
@@ -339,137 +421,20 @@ int main(void)
     // ======== END MANUAL ITM
     
 
-    //*********************************************
-    //             INITIALIZE ASIMPLE SPI
-    //*********************************************
-    
-    //Manual SPI Init
-    //init_iom();
 
-
-    // Asimple spi init
-    struct spi_bus* spi_bus_0;
-    struct spi_device* lora_device;
-
-    // Init LORA as spi device, 10kHz
-    spi_bus_0 = spi_bus_get_instance(SPI_BUS_0);
-    lora_device = spi_device_get_instance(spi_bus_0, SPI_CS_0, 10000);
-
-    if(!spi_bus_enable(spi_bus_0)) { report(-1); }
-
-    tmp_read_reg(lora_device, 0x01);
+    am_util_stdio_printf("Initialization complete\n");
 
     //*********************************************
     //          END OF INITIALIZATION
     //*********************************************
-
+    
     //am_util_stdio_terminal_clear();
-    am_util_stdio_printf("Hello World! (Over UART!)\n\n");
+    am_util_stdio_printf("Hello World! (Over UART!!)\n\n");
     //am_hal_uart_tx_flush(phUART);
-
-
-    bool led_state = false;
     
-    // while(1){
-    //     am_util_stdio_printf("Hello!\n");
+    // Go into one of our test program (these dont return!)
 
-    //     led_state = !led_state;
-    //     set_leds(led_state);
-
-    //     am_util_delay_ms(2000);
-
-    // } // TODO TEMP: CHECK PRINTF
-    
-
-    //===== setup iom, read some regs
-    am_util_stdio_printf("READING INITIAL REGOPMODE\n");
-    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", 1, tmp_read_reg(lora_device, 0x01));
-    am_util_stdio_printf("lora version (@0x%x): 0x%x\n", 0x42, tmp_read_reg(lora_device, 0x42));
-
-    //===== try resetting the Lora module
-    am_util_stdio_printf("RESETTING LORA MODULE:\n");
-    am_hal_gpio_state_write(JV_PIN_LORA_RST, AM_HAL_GPIO_OUTPUT_CLEAR);
-    am_util_delay_ms(50);
-    am_hal_gpio_state_write(JV_PIN_LORA_RST, AM_HAL_GPIO_OUTPUT_SET);
-    am_util_delay_ms(50);
-
-    //am_util_stdio_printf("READING LORA REGS:\n");
-    //for (int reg = 0; reg < 8; reg++) {
-    //    int res = spi_read_lora_reg(reg);
-    //    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", reg, res);
-    //}
-
-    am_util_stdio_printf("Trying burst read LORA REGS:\n");
-    spi_read_lora_nregs(0x41, 0x42); //read first 15 bytes? skipping addr 0
-
-    am_util_stdio_printf("Trying Wakeup\n");
-    tmp_write_reg(lora_device, 0x1, 0x89); //RegOpMode, set to sleep
-    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", 1, tmp_read_reg(lora_device, 1));
-
-    am_util_stdio_printf("Enabling LORA MODE\n");
-    tmp_write_reg(lora_device, 0x1, 0x08); //RegOpMode, set to sleep
-    am_util_stdio_printf("lora reg 0x%x: 0x%x\n", 1, tmp_read_reg(lora_device, 1));
-    tmp_write_reg(lora_device, 0x1, 0x88); //RegOpMode, enable lora
-    tmp_write_reg(lora_device, 0x1, 0x89); //RegOpMode, wake to stdby mode
-    //set to lora mode (bit 7)
-    //set to Low freq mode (bit3 hi)
-    //set to stdby mode (low bits 001)
-    am_util_stdio_printf("READING LORA REGS:\n");
-    for (int reg = 0; reg < 8; reg++) {
-        //int res = spi_read_lora_reg(reg);
-        int res = tmp_read_reg(lora_device, reg);
-        am_util_stdio_printf("lora reg 0x%x: 0x%x\n", reg, res);
-    }
-
-
-    init_morse_table();
-
-    set_leds(led_state);
-
-    // === Setup Morse
-    char *morse_str = " Test 1 2 3. the war of 1812. jabberwocky."
-                      " we hold these truths to be self evident. "
-                      " 4 score and 7 years ago."
-    " to be or not to be, that is the question. whether tis nobler in the mind"
-    " to suffer the slings and arrows of outrageous fortune, or to take arms"
-    " against a sea of troubles and by opposing end them"
-    ;
-
-    // Initialize morse_state to be at first phase of first char of str
-    struct morse_state state = {0};
-    state.p_str = morse_str;
-
-    while (1) {
-        //am_hal_gpio_state_write(JV_CS_PIN, AM_HAL_GPIO_OUTPUT_TOGGLE);
-
-        int last_offset = state.char_offset;
-        struct morse_output result = morseAdvance(&state);
-        if((int)state.char_offset != last_offset) {
-            am_util_stdio_printf("%c", state.p_str[last_offset]);
-        }
-
-
-    
-        if(result.valid) {
-            set_leds(result.out_high);
-            am_util_delay_ms(result.duration_ms);
-
-        } else { //end of string, reset
-            state.char_offset = 0;
-            am_util_delay_ms(BLINK_PERIOD);
-        }
-    }
-
-    // Blink forever
-    //while (1)
-    //{
-    //    // Toggle LEDs
-    //    led_state = !led_state;
-    //    set_leds(led_state);
-
-    //    // Delay
-    //    am_util_delay_ms(BLINK_PERIOD);
-
-
-    //}
+    //testprog_helloblinky();
+    testprog_lora();
+    //testprog_morse();
 }
